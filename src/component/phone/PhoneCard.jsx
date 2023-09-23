dd49c33c3bebb3b38cba64003485677e3c7f138e
MaLiHa1321 <malihasultana2001@gmail.com>
2023-09-23 23:31:37 +0600

    show all btn done

--- src/component/phone/PhoneCard.jsx
+++ src/component/phone/PhoneCard.jsx
@@ -1,7 +1,41 @@
-import { Link } from "react-router-dom";
+import swal from 'sweetalert';
+
 
 const PhoneCard = ({phone}) => {
+   
     const {id,image,phone_name,price,rating,brand_name} = phone || {};
+
+    const handleFavourites= () =>{
+      
+
+        const addedFavo =[];
+
+        const favoriteItem = JSON.parse(localStorage.getItem('favo')) ;
+
+        if(!favoriteItem){
+            addedFavo.push(phone) 
+            localStorage.setItem('favo',JSON.stringify(addedFavo))
+
+        }
+        else{
+            const exist = favoriteItem.find(phone => phone.id === id)
+            console.log(exist)
+            if(!exist){
+                addedFavo.push(...favoriteItem,phone)  
+                localStorage.setItem('favo',JSON.stringify(addedFavo))
+                swal("Good job!", "You've added the item")
+
+            }
+            else{
+                swal("Error", "You lready added this item", "error")
+              return;
+            }
+       
+
+        }
+
+        
+    }
     return (
         <div>
              <div className="card w-78 bg-base-100 shadow-xl">
@@ -12,9 +46,9 @@ const PhoneCard = ({phone}) => {
     <p>Price:{price}Tk</p>
     <p>Rating:{rating}</p>
     <div className="card-actions ">
-        <Link to={`/phones/${id}`}>
-        <button className="btn btn-primary w-full">Favourite</button>
-        </Link>
+     
+        <button onClick={handleFavourites} className="btn btn-primary w-full">Favourite</button>
+       
     
     </div>
   </div>
--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from "react";
+import PhoneCard from "../component/phone/PhoneCard";
 
 
 const Products = () => {
+
+    const [favorite, setFavorite] = useState([]);
+    const [notFound, setFound] = useState("");
+    const [total, setTotal] = useState(0);
+    const [show, setShow] = useState(false);
+
+    useEffect(()=>{
+        const favoriteItem = JSON.parse(localStorage.getItem('favo')) ;
+        if(favoriteItem){
+            setFavorite(favoriteItem)
+        }
+        else{
+           setFound("No data found")
+        }
+    
+
+    },[])
+
+    const handleDelete= () =>{
+        localStorage.clear()
+        setFavorite([])
+        setFound("No data found")
+    }
+
+
+    console.log(favorite)
     return (
         <div>
-            <h1>this is produvt</h1>
+
+            <h1>this is product: {favorite.length}</h1>
+            {
+                notFound? <p className="w-11/12 mx-auto">{notFound}</p>: <div>
+                    {favorite.length >0 && <button onClick={handleDelete} className="btn">Delete</button>}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        
+                        {
+                            show?  favorite.map(phone => <PhoneCard phone={phone}></PhoneCard>):
+
+                        favorite.slice(0,4).map(phone => <PhoneCard phone={phone}></PhoneCard>)}</div>
+                </div>
+
+               
+            }
+
+<button onClick={()=>setShow(!show)} className="btn btn-primary m-5">See more</button>
+
+           
             
         </div>
     );
